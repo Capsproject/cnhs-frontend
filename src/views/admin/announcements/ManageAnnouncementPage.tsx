57f0f6b38e5f58affd5903dbb69be9e3eff1573c
KuyaGit <marklowelmontealto14@gmail.com>
2024-06-30 23:43:35 +0800

    Added Card in Announcements and Events

--- src/views/admin/announcements/ManageAnnouncementPage.tsx
+++ src/views/admin/announcements/ManageAnnouncementPage.tsx
@@ -1,5 +1,28 @@
+import { Card, Avatar } from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import Meta from "antd/es/card/Meta";
+
 const ManageAnnouncementPage: React.FC = () => {
-    return <div>ManageAnnouncementPage</div>;
+    return <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
 }
 
 export default ManageAnnouncementPage;
--- src/views/admin/events/ManageEventPage.tsx
+++ src/views/admin/events/ManageEventPage.tsx
@@ -1,7 +1,30 @@
+import { Avatar, Card } from "antd";
 import React from "react";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import Meta from "antd/es/card/Meta";
+
 
 const ManageEventPage : React.FC = () => {
-    return <div>ManageEventPage</div>;
+    return <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
 }
 
 export default ManageEventPage;
