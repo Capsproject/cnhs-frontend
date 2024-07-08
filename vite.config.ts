2e865ace45eae19aac3cb278c486114100f68c39
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-07-08 16:24:55 +0800

    updated vite.config.ts proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +11,12 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-})
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "https://localhost:8000/api/v1",
+        changeOrigin: true,
+      },
+    },
+  },
+});
